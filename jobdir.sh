441f0876b1f893ec21a6133423ec2496e9095421
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-11-15 17:16:04 -0800

    Update jobdir.sh

--- jobdir.sh
+++ jobdir.sh
@@ -2,3 +2,6 @@
 for j in $(qstat hep | grep $1 | tr . ' ' | awk '{print $1}' ) 
   do checkjob -v $j | grep Output | awk '{print $2}' 
 done
+
+
+ qstat -u shg047 | awk '{print $1,$10}' | grep R | grep local | awk '{print $1}' | xargs -n 1 checkjob
